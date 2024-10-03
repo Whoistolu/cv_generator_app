ef0ebfd0e63944d32a8ebbda6f1384efe5dad722
Whoistolu <digittolu25@gmail.com>
2024-10-04 00:00:39 +0100

    Add method to generate a PDF

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -6,5 +6,14 @@ class ResumesController < ApplicationController
   end
 
   def show
+    @resume = Resume.find(params[:id])
+    
+    respond_to do |format|
+      format.html
+      format.pdf do
+        pdf = ResumePdf.new(@resume)
+        send_data pdf.render, filename: "resume.pdf", type: "application/pdf", disposition: "inline"
+      end
+    end
   end
 end
