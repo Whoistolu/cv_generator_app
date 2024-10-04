ceda0ab06d047beac0e94c32357f4dada79d97aa
Whoistolu <digittolu25@gmail.com>
2024-10-04 22:26:05 +0100

    Add the update method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,14 @@ class UsersController < ApplicationController
   def edit
     @user = current_user
   end
+
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      render json: { message: 'Profile updated successfully.' }
+    else
+      render :edit
+    end
+  end
+
 end
