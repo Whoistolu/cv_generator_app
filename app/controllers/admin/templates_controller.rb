c1c1f804cc8a2bf2f3f72f1e8846a90654542cfa
Whoistolu <digittolu25@gmail.com>
2024-10-05 02:11:40 +0100

    Add user validations to authenticate users

--- app/controllers/admin/templates_controller.rb
+++ app/controllers/admin/templates_controller.rb
@@ -1,7 +1,9 @@
 class Admin::TemplatesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :admin_only
 
   def new
     @template = Template.new
   end
-  
+
 end
