61468aa96c353fb25dc5dc81f19441f8b2595484
Whoistolu <digittolu25@gmail.com>
2024-10-05 02:21:05 +0100

    Add the template params method and the Admin only method

--- app/controllers/admin/templates_controller.rb
+++ app/controllers/admin/templates_controller.rb
@@ -6,4 +6,14 @@ class Admin::TemplatesController < ApplicationController
     @template = Template.new
   end
 
+  private
+
+  def template_params
+    params.require(:template).permit(:name, :description, :design_code)
+  end
+
+  def admin_only
+    redirect_to(root_path, alert: 'Not authorized') unless current_user.admin?
+  end
+
 end
