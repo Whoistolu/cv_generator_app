8eb6e61e60c96e95ad1b4c95ae68c9a517e4d96e
Whoistolu <digittolu25@gmail.com>
2024-10-09 22:33:21 +0100

    Add the new and the create action

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -2,12 +2,20 @@ class ResumesController < ApplicationController
   before_action :authenticate_user!
 
   def new
+    @resume = Resume.new
   end
 
   def create
+    @resume = current_user.resumes.build(resume_params)
+    if @resume.save
+      render json: { message: 'Resume created successfully.' }
+    else
+      render :new
+    end
   end
 
   def show
+    @resume = Resume.find(params[:id])
   end
 
   private
