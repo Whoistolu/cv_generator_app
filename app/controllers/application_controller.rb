0a27e2e9aa93e728f1d77b476c19d7864e2802a3
Whoistolu <digittolu25@gmail.com>
2024-10-10 12:42:19 +0100

    Handle Authorization Exceptions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::API
+  rescue_from CanCan::AccessDenied do |exception|
+    render json: { message: 'You are not authorized to perform this action.' }
+  end
 end
